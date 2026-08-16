40d56df7551079e8740d17a1652c75051b0b6186
jaynorton17 <jaynorton17@gmail.com>
2026-08-16 21:30:32 +0100

    Wire live penalties tab and laptop fit styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { AppChromeEnhancements } from './AppChromeEnhancements';
 import './styles.css';
+import './laptop-fit.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <AppChromeEnhancements />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
