98023bd0ce4f2fdf5e15882e89b6b64cbe3eee93
jaynorton17 <jaynorton17@gmail.com>
2026-08-17 02:13:58 +0100

    Load Home analytics graphic styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -23,6 +23,7 @@ import './product-pass.css';
 import './competition-identities.css';
 import './analytics-pass.css';
 import './home-product-pass.css';
+import './home-analytics-graphics.css';
 
 installBookieBallFetchCache();
 
